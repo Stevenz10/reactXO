2708ba1ec396ae6220ce47f1beea820375e07598
Stevenz10 <stevenzheng2002@gmai.com>
2023-09-01 21:02:44 -0500

    Añadiendo función highlight y draw

--- src/Board.jsx
+++ src/Board.jsx
@@ -6,7 +6,7 @@ export default function Board({ xIsNext, squares, onPlay }) {
     const board = []
     let status;
     if (winner) {
-      status = "Winner: " + winner;
+      status = "Winner: " + winner[3];
     } else {
       status = "Next player: " + (xIsNext ? "X" : "O");
     }
@@ -15,16 +15,30 @@ export default function Board({ xIsNext, squares, onPlay }) {
       const cols = [];
       for (let j = 0; j < 3; j++) {
         const id = i * 3 + j;
-        cols.push(
-          <Square
-            key={id}
-            value={squares[id]}
-            onSquareClick={() => handleClick(id, xIsNext)}
-          />
-        );
+        console.log(winner);
+        if (winner && winner.includes(id)) {
+          cols.push(
+            <Square
+              key={id}
+              value={squares[id]}
+              highlight={true}
+              onSquareClick={() => handleClick(id, xIsNext)}
+            />
+          );
       }
-      board.push(<div key={i} className="board-row">{cols}</div>);
+      else{
+        cols.push(
+            <Square
+              key={id}
+              value={squares[id]}
+              highlight={false}
+              onSquareClick={() => handleClick(id, xIsNext)}
+            />
+          );
+      }
     }
+      board.push(<div key={i} className="board-row">{cols}</div>);
+  }
     function handleClick(id,xIsNext) {
         const nextSquares = squares.slice();
         if(squares[id] || calculateWinner(squares)){
@@ -52,7 +66,8 @@ export default function Board({ xIsNext, squares, onPlay }) {
         for (let i = 0; i < lines.length; i++) {
           const [a, b, c] = lines[i];
           if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+            lines[i].push(squares[a]); // [0,1,2,'X'] 
+            return lines[i];
           }
         }
         return null;
@@ -65,5 +80,4 @@ export default function Board({ xIsNext, squares, onPlay }) {
     </div>
   )
 }
-
 
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Square({value,onSquareClick}) {
+export default function Square({value,onSquareClick,highlight}) {
   return (
-    <button className='square' onClick={onSquareClick}>{value}</button>
+    highlight ? (<button className='square highlight' onClick={onSquareClick}>{value}</button>):(<button className='square' onClick={onSquareClick}>{value}</button>)
   )
 }
